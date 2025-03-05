492d34c4f3a9ce3d149ec98d2975e8d73aa734a2
Fikrikunn <ajjafikri1@gmail.com>
2025-03-05 13:01:42 +0700

    main.jsx DONE

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { RoomProvider } from "./context/RoomContext";
+import Home from "./pages/Home";
+import Lobby from "./pages/Lobby"; // Tambahkan import
+import AuthLayout from "./pages/AuthLayout";
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
+const App = () => {
+  return (
+    <RoomProvider>
+      <Routes>
+        <Route element={<AuthLayout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+        <Route path="/lobby" element={<Lobby />} /> {/* Route ke Lobby */}
+      </Routes>
+    </RoomProvider>
+  );
+};
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
